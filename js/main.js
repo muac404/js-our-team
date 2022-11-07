ea43e3d1386acd7636ed5994b3a76441c32e2289
muac404 <muac404@gmail.com>
2022-11-07 15:46:26 +0100

    Aggiunge Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 "use strict"
+
+// Creo l’array di oggetti con le informazioni fornite.
+
+const members = 
+[
+    {
+        fullName : "Wayne Barnett",
+        position : "Founder & CEO",
+        profileImage : "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        fullName : "Angela Caroll",
+        position : "Chief Editor",
+        profileImage : "angela-caroll-chief-editor.jpg"
+    },
+
+    {
+        fullName : "Walter Gordon",
+        position : "Office Manager",
+        profileImage : "walter-gordon-office-manager.jpg"
+    }
+]
+
+console.log(members)
+
+// Stampo su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+
+
+// Stampo le stesse informazioni su DOM sottoforma di stringhe
